daa78f2cb6541d84e1628916b90e4df2df22ced1
folkert van heusden <mail@vanheusden.com>
2023-08-27 17:44:09 +0200

    missing include

--- str.h
+++ str.h
@@ -1,7 +1,8 @@
-// (C) 2021 by Folkert van Heusden <mail@vanheusden.com>
+// (C) 2021-2023 by Folkert van Heusden <mail@vanheusden.com>
 // Released under Apache License v2.0
 
 #include <string>
+#include <vector>
 
 std::string myformat(const char *const fmt, ...);
 
