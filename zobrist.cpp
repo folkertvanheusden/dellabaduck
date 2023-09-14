1d60605021e4761f9f7e4d36849c1a46096e5fc3
folkert van heusden <mail@vanheusden.com>
2023-09-14 17:08:34 +0200

    for SSK (situational superko) we want a color for who-is-about-to-move, both SSK and PSK want different hashes for each stone color

--- zobrist.cpp
+++ zobrist.cpp
@@ -12,11 +12,7 @@ Zobrist::~Zobrist()
 
 void Zobrist::setDim(const int dim)
 {
-#ifdef SITUATIONAL_SUPERKO
 	size_t newN = dim * dim * 2;
-#else
-	size_t newN = dim * dim;
-#endif
 
 	for(size_t i=rngs.size(); i<newN; i++)
 		rngs.push_back(distribution(gen));
@@ -24,9 +20,5 @@ void Zobrist::setDim(const int dim)
 
 uint64_t Zobrist::get(const int nr, const bool black) const
 {
-#ifdef SITUATIONAL_SUPERKO
 	return rngs.at(nr * 2 + black);
-#else
-	return rngs.at(nr);  // TT & CGOS
-#endif
 }
