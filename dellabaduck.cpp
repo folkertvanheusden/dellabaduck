9b9b999112aa5eae353430bb5312237b2f3b7093
folkert van heusden <mail@vanheusden.com>
2023-10-19 07:58:12 +0200

    layout

--- dellabaduck.cpp
+++ dellabaduck.cpp
@@ -392,13 +392,13 @@ int main(int argc, char *argv[])
 			auto   liberties   = b->findLiberties(player);
 			int    n_liberties = liberties.size();
 
-			auto s = score(*b, komi);
-			double current_score = player == board_t::B_BLACK ? s.first - s.second : s.second - s.first;
-			bool pass_for_win_time = current_score > 0 && time_left <= 0;  // we've won anyway
+			auto   s = score(*b, komi);
+			double current_score     = player == board_t::B_BLACK ? s.first - s.second : s.second - s.first;
+			bool   pass_for_win_time = current_score > 0 && time_left <= 0;  // we've won anyway
 
 			// at this stage of the development we should be happy with draw
 			// later change "current_score >= " to "... > "
-			bool pass_for_win_pass = pass > 0 && current_score >= 0;
+			bool   pass_for_win_pass = pass > 0 && current_score >= 0;
 
 			if (n_liberties == 0 || pass_for_win_time || pass_for_win_pass) {
 				send(false, "=%s pass", id.c_str());
