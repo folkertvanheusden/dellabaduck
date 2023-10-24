cb093645cb671c6072f646dece4542762552d5a0
folkert van heusden <mail@vanheusden.com>
2023-10-24 13:21:56 +0200

    nullptr fix

--- dellabaduck.cpp
+++ dellabaduck.cpp
@@ -294,7 +294,7 @@ int main(int argc, char *argv[])
 				sgf += myformat(";%c[pass]", p == board_t::B_BLACK ? 'B' : 'W');
 			}
 			else {
-				uct_node *new_node = root_node->find_position(*b);
+				uct_node *new_node = root_node ? root_node->find_position(*b) : nullptr;
 				delete root_node;
 				root_node = new_node;
 
